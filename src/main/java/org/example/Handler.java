7852bec1568af5bb1081f27682edd697b392eebf
Michail Evseev <mixaevseev1989@gmail>
2023-06-02 22:36:54 +0300

    Initial commit

---
+++ src/main/java/org/example/Handler.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+
+@FunctionalInterface
+public interface Handler {
+    public void handle(Request request, BufferedOutputStream responseStream) throws IOException;
+}
